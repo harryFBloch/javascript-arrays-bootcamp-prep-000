df9190b212a2ca0fa7a31bd89f6a4c8a4eef7c5d
Harry Bloch <harryfbloch@gmail.com>
2018-08-27 16:51:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,3 +17,8 @@ function addElementToEndOfArray(array, element) {
 function destructivelyAddElementToEndOfArray(array, element){
   return [...array, element]
 }
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
