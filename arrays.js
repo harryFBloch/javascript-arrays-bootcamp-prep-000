d5489af31ef0909ce13c247b7768fc3520979975
Harry Bloch <harryfbloch@gmail.com>
2018-08-27 16:33:55 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers","hudred grand","kitkat","skittles"]
+
+function addElementToBeginningOfArray(array,element) {
+  
+}
+
+function destructivlyAddElementToBeginningOFArray(){
+  
+}
