51501221605dcf8805c601cb4fdd56e8b1f9f759
Harry Bloch <harryfbloch@gmail.com>
2018-08-27 16:58:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,4 +31,11 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
 
-function
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array 
+}
+
+function removeElementFromEndOfArray(array){
+  var newArray = array.slice(0, array.length - 1)
+}
